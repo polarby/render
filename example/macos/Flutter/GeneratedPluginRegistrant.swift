c221b446fd5a8be0efee12d4e4ec0f2301ffb715
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-12-19 21:22:53 +0200

    fix: macos example and video player

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import ffmpeg_kit_flutter_https_gpl
 import path_provider_foundation
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
